6a0f054fb19def224da98db475b90dd7f1c2b8b5
EoPi <tinykidpro@gmail.com>
2022-07-03 16:16:23 +0700

    Update index.4fe17385.js

--- assets/index.4fe17385.js
+++ assets/index.4fe17385.js
@@ -906,7 +906,7 @@ const ie = () => {
                     className: "videoContainer2",
                     children: e(N, {
                         className: "react-player",
-                        url: "//www.youtube.com/embed/GrG2-oX5z24?autoplay=1&mute=1&start=20",
+                        url: "//www.youtube.com/embed/Xry_g0Vp5MY?autoplay=1&mute=1&start=20",
                         width: "100%",
                         height: "100%",
                         position: "relative",
